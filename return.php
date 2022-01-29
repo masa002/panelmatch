3313e801d44fb3684fbe43b543c1895620894672
masa002 <89620605+masa002@users.noreply.github.com>
2022-01-29 18:18:49 +0900

    Add files via upload

---
+++ return.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+require "db_connect.php";
+$sql = "INSERT INTO pm(name,pass) VALUES(:name,:pass)";
+
+//アカウント登録signin
+//被った名前を探す
+$sql2 = "select name from pm";
+$stm2 = $pdo->prepare($sql2);
+$stm2->execute();        
+$result2 = $stm2->fetchAll(PDO::FETCH_ASSOC);
+
+if( $result2!=null ) {
+    foreach($result2 as $data) {
+        if(isset($_POST["name"])) {
+            if($data["name"] === $_POST["name"]) {
+                $_SESSION["errname"]=1;
+                header("location:signin.php");
+            } else {
+                $_POST["name"] = htmlspecialchars($_POST["name"],ENT_QUOTES,"UTF-8");
+                $name = $_POST["name"];
+                $_SESSION['name'] = $_POST['name'];
+            }
+        }
+    }
+} else {
+    $_POST["name"] = htmlspecialchars($_POST["name"],ENT_QUOTES,"UTF-8");
+    $name = $_POST["name"];
+    $_SESSION['name'] = $_POST['name'];
+}
+
+if(isset($_POST["pass"])){
+    $_POST["pass"] = htmlspecialchars($_POST["pass"],ENT_QUOTES,"UTF-8");
+    if(preg_match('/\w{8,}/u',$_POST["pass"]) == 1) {
+        $pass = hash("sha256",$_POST["pass"]);
+    } else {
+        $_SESSION["errpass"]=1;
+        header("location:signin.php");
+    }
+}
+
+if(isset($name)==1 && isset($pass)==1 ) {
+    $stm = $pdo->prepare($sql);//プリペアードステートメントを作成
+    $stm->bindValue(":name",$name,PDO::PARAM_STR); 
+    $stm->bindValue(":pass",$pass,PDO::PARAM_STR); 
+    $stm->execute();        //sqlの実行
+    echo "アカウント登録が完了しました！！"."<br>";
+    echo '<label>'.'<a href="head.php">'.'トップ画面へ'.'</a>'.'</label>'; //今だけ<a head.php>修正➝title.php
+}
+
+
+
+
+
+
+
+
+
+
+//ここから下はlogin.phpの処理
+$home=2;//0になったらログイン成功
+//被った名前を探す
+$sql3 = "select name from pm";
+$stm3 = $pdo->prepare($sql3);
+$stm3->execute();        
+$result3 = $stm3->fetchAll(PDO::FETCH_ASSOC);
+//一致するpassを探す
+$sql4 = "select pass from pm";
+$stm4 = $pdo->prepare($sql4);
+$stm4->execute();        
+$result4 = $stm4->fetchAll(PDO::FETCH_ASSOC);
+
+//登録されたname,passと$_POST["namelog"],$_POST["passlog]が同じならログイン
+$sql5 = "select pass from pm where name = :namelog";
+
+
+if(isset($_POST["namelog"])){
+    foreach($result3 as $data3) {
+        if($data3["name"] === $_POST["namelog"]) {
+            echo 'データベースにありました';
+            $setname = htmlspecialchars($_POST["namelog"],ENT_QUOTES,"UTF-8");
+            unset($_SESSION['errnamelog']);
+            $stm5 = $pdo->prepare($sql5);
+            $stm5->bindValue(":namelog",$_SESSION["name"],PDO::PARAM_STR);
+            $stm5->execute();
+            $result5 = $stm5->fetch(PDO::FETCH_ASSOC);
+            var_dump($result5);
+            break;
+        } else{
+            $_SESSION["errnamelog"]=1;
+        }
+    }
+    if(isset($_SESSION['errnamelog']) ) {
+        echo 20;
+        header("location:login.php");
+    }
+}
+
+if(isset($_POST["passlog"])) {
+    echo 'post';
+    $_POST["passlog"] = htmlspecialchars($_POST["passlog"],ENT_QUOTES,"UTF-8");
+    if(preg_match('/\w{8,}/u',$_POST["passlog"]) == 1) {
+        echo 'preg_match';
+        $_POST["passlog"] = hash("sha256",$_POST["passlog"]);
+        foreach($result4 as $data4){
+            echo 'foreach';
+            $op =hash_equals( $data4["pass"],$_POST["passlog"]);
+            echo $op;
+            if( hash_equals( $data4["pass"],$_POST["passlog"])==1 ) {
+                if($result5["pass"] == $_POST["passlog"]){
+                    header("location:head.php");
+                }
+            }else{
+                $_SESSION["errpasslog"]="パスワードが違います";
+                    header("location:login.php");
+            }
+        }
+    } else{$_SESSION["errnamelog"]=1; header("location:login.php");} //英数字８以上でなければやり直し
+}
+echo $home;
+if($home <= 0) {
+    header("location:head.php");//ホームへ//今だけ<a head.php>修正➝title.php
+}
+
+?>
---
+++ signin.php
@@ -0,0 +1,118 @@
+<?php
+session_start();
+?>
+<html>
+    <head>
+    <meta charset="UFT-8">
+        <title>アカウント登録</title>
+        <style>
+            /* header 部分 */
+            .h_body { 
+                margin: 0;
+                font-family: "ヒラギノ角ゴシック体 W3";
+            }
+            .header {
+                display: flex;
+                justify-content: space-between;
+                margin: 0 auto;
+                font-size: 1.3rem;
+                background-color: #ffd958;
+                padding-left: 20%;
+                padding-right: 15%;
+            }
+            .header a {
+                text-decoration: none;
+                color: black;
+            }
+            .header a:hover { text-decoration: underline; }
+            .h_top { display: flex; transition: 0.3s; }
+            .h_top:hover { transform: scale(1.2,1.2); }
+            .h_logout { display: flex; }
+            .h_img1 {
+                width: 30px;
+                height: 30px;
+                margin-top: 20px;
+                margin-right: 5px;
+            }
+            .h_in { display: flex; }
+            .h_login a {
+                display:flex;
+                margin-right: 30px;
+            }
+            .h_img2 {
+                width: 28px;
+                height: 28px;
+                margin-top: 23px;
+                margin-right: 5px;
+            }
+            .h_logou { margin-left: 30px; transition: 0.3s; }
+            .h_logou:hover { transform: scale(1.2,1.2); }
+            .h_login { transition: 0.3s; }
+            .h_login:hover { transform: scale(1.2,1.2); }
+            .h_signin { transition: 0.3s; }
+            .h_signin:hover { transform: scale(1.2,1.2); }
+
+            /* signin部分 */
+            .signin {
+                margin-top: 5vw;
+                margin-left: 35vw;
+            }
+            .signin p {
+                font-size: 1.3vw;
+            }
+            .signin a { font-size: 12px; }
+            .sigsub { margin-left: 12vw; }
+        </style>
+    </head>
+    <body class="h_body">
+	<header class="header">       
+	    <a href="head.php" class="h_topa">   <!-- 今だけaタグの中身 head.php➝修正title.php -->
+            <div class="h_top">
+                <img src="images/home.png" class="h_img1" alt="家の画像" >
+                <p>トップ</p>
+            </div>
+        </a>
+        <?php   //右側の表示
+        //ログインしている状態かどうか、している
+        if( isset($_SESSION['name']) ) {      //SESSIONに名前があるか   //今だけaタグの中身 head.php➝修正title.php ↓
+            echo "<div class='h_logout'><p>".$_SESSION['name']."さん</p><p class='h_logou'><a href='head.php'>ログアウト</a></p></div>";
+            unset($_SESSION['name']);
+        //ログインしていないとき
+        } else {  
+            echo "<div class='h_in'>
+                    <div class='h_login'>
+                        <a href='login.php'><img src='images/loginicon.svg' class='h_img2' alt='アイコン'>
+                        <p>ログイン</p></a>
+                    </div>" ;
+            echo   "<div class='h_signin'><p><a href='signin.php'>アカウント作成</a></p></div>
+                 </div>";
+        }
+        ?>
+	</header>
+    <div class="signin">    
+    <form action="return.php" method="POST" >
+        <h2>アカウント登録</h2>
+
+        <p>ユーザー名<?php if(isset($_SESSION["errname"])){echo '<a style="color:#ff0000;font-size: 12px;">　　　　　　もう使われている名前だよ？</a>';}?></p>
+        <input type="text" name="name" style="width: 30vw" required><br><br>
+
+
+        <p>パスワード<?php if(isset($_SESSION["errpass"])){echo '<a style="color:#ff0000";font-size: 12px;>　　　　　　アルファベットと数字だけで8文字以上書いてね？</a>';}?></p>
+        <input type="password" name="pass" placeholder="半角英数のみ8文字以上" style="width: 30vw" required><br><br>
+
+        <input type="submit" value="登録" class="sigsub">
+
+    </form>
+    </div>
+    </body>
+</html>
+
+<script>    
+window.onload = function(){
+document.getElementById("mainForm").onsubmit = function(){
+<?php session_destroy();
+//submitが押されたらsessionを破棄
+?>
+}
+}
+</script>
