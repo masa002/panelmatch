e6f9d5a41879cd45232c666cabaed601f914c46f
masa002 <m.obara.sys21@morijyobi.ac.jp>
2022-01-30 11:17:35 +0900

    Update profile.php

--- profile.php
+++ profile.php
@@ -9,12 +9,15 @@
     echo $_SESSION["name"]."<br>";
     echo "ベストスコア";
 
-    $sql = "SELECT * FROM pm WHERE name = :name";
+    $sql = "SELECT score FROM pm WHERE name = :name";
 
     // プリペアドステートメントを作成する
     $stm = $pdo->prepare($sql);
     // プレースホルダに値をバインドさせる
-    $stm->bindValue(':', $_SESSION["name"], PDO::PARAM_STR);
+    $stm->bindValue(':name', $_SESSION["name"], PDO::PARAM_STR);
     // SQLを実行する
     $stm->execute();
+
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    echo $result;
 ?>
