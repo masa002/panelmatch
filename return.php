cddcf38a6f211e40e4624c247eebf5b3522fc58f
hakkin-1219 <silverfile@icloud.com>
2022-01-30 15:03:17 +0900

    ｋ

--- return.php
+++ return.php
@@ -15,27 +15,23 @@ unset($_SESSION["errname"]);
 unset($_SESSION["errpass"]);
 unset($_SESSION["errnamelog"]);
 unset($_SESSION["errpasslog"]);
-
-
-
-if( $result2!=null ) {
-    if(isset($_POST["name"])) {
+// $result2 -> select name from pm
+if(isset($_POST["name"])) {
+    if( $result2!=null ) {
         foreach($result2 as $dust){
-            if($dust["name"] === $_POST["name"]){
-             $_SESSION["errname"]="もう使われている名前だよ？";
-             header("location:signin.php");
-           } else {
+            if($dust["name"] === $_POST["name"]) {
+                $_SESSION["errname"]="もう使われている名前だよ？";
+                header("location:signin.php");
+            } else {
                 $name = htmlspecialchars($_POST["name"],ENT_QUOTES,"UTF-8");
                 $nerr = 0;
             }
+        }
+    } else {
+        $name = htmlspecialchars($_POST['name'],ENT_QUOTES,"UTF-8" );
+        $nerr = 0;
     }
-    }else{$_SESSION["errname"]="名前を書いてください";header("location:signin.php");}
-} else {
-    $_POST["name"] = htmlspecialchars($_POST["name"],ENT_QUOTES,"UTF-8");
-    $name = $_POST["name"];
-    $nerr = 0;
 }
-
 if(isset($_POST["pass"])){
     $_POST["pass"] = htmlspecialchars($_POST["pass"],ENT_QUOTES,"UTF-8");
     if(preg_match('/\w{8,}/u',$_POST["pass"]) == 1) {
@@ -46,7 +42,6 @@ if(isset($_POST["pass"])){
         header("location:signin.php");
     }
 }
-
 if($perr != 1 && $nerr != 1 ) {
     $stm = $pdo->prepare($sql);//プリペアードステートメントを作成
     $stm->bindValue(":name",$name,PDO::PARAM_STR); 
@@ -56,14 +51,6 @@ if($perr != 1 && $nerr != 1 ) {
     $_SESSION["signin"] = 1;
     header("location:signin.php");
 }
-
-
-
-
-
-
-
-
 //ここから下はlogin.phpの処理
 $home=2;//0になったらログイン成功
 //被った名前を探す
@@ -74,12 +61,7 @@ $result3 = $stm3->fetchAll(PDO::FETCH_ASSOC);
 //一致するpassを探す
 $sql4 = "select pass from pm where name = :namelog";
 $stm4 = $pdo->prepare($sql4);
-
-
 //登録されたname,passと$_POST["namelog"],$_POST["passlog]が同じならログイン
-
-
-
 if(isset($_POST["namelog"])){
     foreach($result3 as $data3) {
         if($data3["name"] === $_POST["namelog"]) {
@@ -92,7 +74,6 @@ if(isset($_POST["namelog"])){
         } else{
             $_SESSION["errnamelog"]='';
              header("location:login.php");
-
         }
     }
     if(isset($_SESSION['errnamelog']) ) {
@@ -100,7 +81,6 @@ if(isset($_POST["namelog"])){
         header("location:login.php");
     }
 }
-
 if(isset($_POST["passlog"])) {
     $_POST["passlog"] = htmlspecialchars($_POST["passlog"],ENT_QUOTES,"UTF-8");
     if(preg_match('/\w{8,}/u',$_POST["passlog"]) == 1) {
@@ -111,11 +91,7 @@ if(isset($_POST["passlog"])) {
             }else{
                 $_SESSION["errpasslog"]="パスワードが違います";
                 header("location:login.php");}
-
             
     } else{$_SESSION["errpasslog"]="アルファベットと数字だけで8文字以上書いてね？"; header("location:login.php");} //英数字８以上でなければやり直し
 }
-
-
-
 ?>
