d76e82ed541868d7f283816ba6b5f572f41e161a
masa002 <89620605+masa002@users.noreply.github.com>
2022-01-30 15:00:43 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 $user = "root";
 $pass="i0GKtpBFg7hcOTLcQPf6UETxnICFkRTl";
 $database="pm";
-$server="dbwx0a.stackhero-network.com";
+$server="dbwx0a.stackhero-network.com:3306";
 
 //DSN文字列の生成
 $dsn = "mysql:host={$server};dbname={$database};cherset=utf8";
