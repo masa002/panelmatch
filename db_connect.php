a87c859ff9923777acbfe735cca9a30826f2e6d6
masa002 <89620605+masa002@users.noreply.github.com>
2022-01-30 14:46:29 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 //ユーザー名、パスワード、データベース名、サーバー
 $user = "root";
-$pass="7mFHARaTxuSA5mzpMklIFh5M37N0JZId";
+$pass="i0GKtpBFg7hcOTLcQPf6UETxnICFkRTl";
 $database="php_jugyo";
 $server="127.0.0.1:3308";
 
